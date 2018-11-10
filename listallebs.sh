77cfa8bd6e07447c7bb074e9a4234669b1d6da28
mrdoro <mrdoro@me.com>
2018-11-10 22:24:08 +0100

    v2

---
+++ listallebs.sh
@@ -0,0 +1,16 @@
+# To use this tool you need to installed and configured AWS CLI!!!
+
+if [ -z "$1" ];
+then
+  echo "No aws cli profile name provided. You can add the name after space: listallec2 profilename\n"
+  ACC=${VARIABLE:-default}
+else
+  ACC=$1
+fi
+echo "Listing all EBS disks for: "$ACC" profile\n"
+
+for region in `aws ec2 describe-regions --output text | cut -f3`
+do
+    echo "\nList of EBS disks in region:'$region':"
+    aws ec2 describe-volumes --query "Volumes[*].{AZ:AvailabilityZone,ID:VolumeId,Type:VolumeType,State:State,Name:Tags[0].Value}" --output=table --region $region --profile $ACC
+done
